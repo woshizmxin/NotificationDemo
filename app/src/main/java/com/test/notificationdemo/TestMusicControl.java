35acf8218da17fd68c5352a0ecb356db564a0537
周茂 <zhoumao@meituan.com>
2015-12-20 13:25:42 +0800

    first commit

---
+++ app/src/main/java/com/test/notificationdemo/TestMusicControl.java
@@ -0,0 +1,16 @@
+package com.test.notificationdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class TestMusicControl extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		intent.getBooleanExtra("play", true);
+		Toast.makeText(context, "music is playing", Toast.LENGTH_SHORT).show();
+	}
+
+}
